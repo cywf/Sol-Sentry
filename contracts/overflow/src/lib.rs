3681cbb8ce40d4dc49246b53fcd58ec3f3aa72bc
Kylo P <100837335+cywf@users.noreply.github.com>
2024-01-14 19:17:28 -0400

    Update lib.rs

---
+++ contracts/overflow/src/lib.rs
@@ -0,0 +1,13 @@
+use solana_program::{
+    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult,
+    pubkey::Pubkey,
+};
+
+entrypoint!(process_instruction);
+
+pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    _instruction_data: &[u8],
+) -> ProgramResult {
+    // overflow logic
